4395dae3b4a9f0ce53a267de04834eaabc8fdfca
root <santhosh.annam@devrabbit.com>
2019-08-23 17:22:25 +0530

    dffgd

---
+++ src/test/java/web/feas/sdrtg/webpageclasses/Sdf.java
@@ -0,0 +1,34 @@
+package web.feas.sdrtg.webpageclasses;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import com.utilities.BaseClass;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+@SuppressWarnings("unused")
+public class Sdf extends BaseClass {
+	
+	public static String primaryInfo  = "{\"user_id\":85,\"executed_user_id\":85,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbit.com/\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"feas\",\"project_description\":\"\",\"project_id\":419,\"module_name\":\"sdrtg\",\"module_description\":\"\",\"sub_module_id\":0,\"module_id\":710,\"testcase_name\":\"fdgfhjf\",\"testcase_id\":577,\"testset_id\":0,\"executed_timestamp\":-1101919925,\"browser_type\":\"chrome\",\"testcase_overwrite\":true,\"client_timezone_id\":\"Asia/Calcutta\"}";
+
+	public static String projectName = "feas";
+	public WebDriver driver;
+	public ExtentReports reports;
+	public ExtentTest test;
+	public static final int datasetsLength = 1;
+
+	public Sdf(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	@FindBy(how = How.XPATH, using = "//a[contains(text(),'Contact')]")	
+	private WebElement	Contact_404684a;
+	public String clkaContact_404684() {
+		waitForExpectedElement(driver, Contact_404684a);		
+		String text = Contact_404684a.getText();
+		Contact_404684a.click();
+		return text;
+	}
+
+}
---
+++ src/test/java/web/feas/sdrtg/webtestclasses/Fdgfhjf.java
@@ -0,0 +1,91 @@
+package web.feas.sdrtg.webtestclasses;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.configurations.Constants;
+import com.configurations.ExtentConfigurations;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import web.feas.sdrtg.webpageclasses.Sdf;
+import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.utilities.Utilities;
+import org.json.JSONObject;
+
+@SuppressWarnings("unused")
+public class Fdgfhjf extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	private String browserName = "chrome";
+	public boolean isElementDispalyed = false;
+	public static final int datasetsLength = 1;
+
+	public Fdgfhjf() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(Fdgfhjf.class);
+		configFileObj = new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("fdgfhjf.properties");
+	}
+
+	@BeforeTest
+	@Parameters("browser")
+	public void browserName(String browser) throws Exception {
+		browserName = browser;
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath, Sdf.projectName);
+		test = reports.startTest(setTestcaseName(browserName, "Fdgfhjf"));
+	}
+
+	
+	public void setUP() throws Exception {
+		String primaryInfo = Sdf.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		driver = launchBrowser(browserName, configFileObj);
+		printSuccessLogAndReport(test, logger, "Browser Name : " + browserName);
+	}
+
+	public void SdfTest(int i) throws Exception {
+
+	 try{
+		Constants.TOTAL_TC = Constants.TOTAL_TC + 1;
+		Constants.IS_TESTCASE = true; Constants.iS_WEB = true;
+		int datasetScreencount = 1;
+		Sdf objSdf = PageFactory.initElements(driver, Sdf.class);
+		testLogHeader(test, "Verify Sdf page");
+		String text1 = objSdf.clkaContact_404684();
+		if(text1.equalsIgnoreCase(configFileObj.getProperty("Contact"+ i + datasetScreencount))){
+			printSuccessLogAndReport(test, logger,  "Clicked on : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+			printSuccessLogAndReport(test, logger,  "Validated Link Text : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+		} else {
+			printFailureLogAndReport(test, logger,  "Link Text is not displayed  : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+		}
+
+	   } catch (Exception e) {
+		  isElementDispalyed = false;
+		  printFailureLogAndReport(test, logger,  "Element is not found" + e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void screensTest() throws Exception {
+		for(int datasets = 1; datasets <= Sdf.datasetsLength; datasets++) {
+			isElementDispalyed = true;			
+			setUP();
+			if(isElementDispalyed) { SdfTest(datasets);}
+			tearDown();
+		}	}
+
+	
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+		driver.quit();
+	}
+}
